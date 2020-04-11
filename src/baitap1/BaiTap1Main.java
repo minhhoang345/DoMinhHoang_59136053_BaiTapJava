66a7a9fd18c94e4491727d8a7f8fc227b91e5de5
DELL <minhhoang345678@gmail.com>
2020-04-11 17:09:11 +0700

    Đỗ Minh Hoàng - Bài Tập Java

--- src/baitap1/BaiTap1Main.java
+++ src/baitap1/BaiTap1Main.java
@@ -9,6 +9,15 @@ package baitap1;
  *
  * @author DELL
  */
-public class BaiTap1Main {
-    
+public class BaiTap1Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("--Nhân viên 1--");
+        NhanVien nv1 = new NhanVien("Đỗ Minh Hoàng", 21, "15/12 Quốc Tuấn", 7000000, 300);
+        System.out.println(nv1.getThongTin());
+        System.out.println("--Nhân viên 2--");
+        NhanVien nv2 = new NhanVien("Phạm Văn Pháp", 21, "213 New York", 3500000, 100);
+        System.out.println(nv2.getThongTin());
+    }
 }
---
+++ src/baitap1/NhanVien.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap1;
+
+/**
+ *
+ * @author DELL
+ */
+public class NhanVien 
+{
+    String ten;
+    int tuoi;
+    String diachi;
+    double tienluong;
+    int tonggiolam;
+
+
+    public NhanVien()
+    {
+    }
+
+    public NhanVien(String ten, int tuoi, String diachi, double tienluong, int tonggiolam)
+    {
+        this.ten = ten;
+        this.tuoi = tuoi;
+        this.diachi = diachi;
+        this.tienluong = tienluong;
+        this.tonggiolam = tonggiolam;
+    }
+
+    public double getTienluong() {
+        return tienluong;
+    }
+
+    public void setTienluong(double tienluong) {
+        this.tienluong = tienluong;
+    }
+
+    public int getTonggiolam() {
+        return tonggiolam;
+    }
+
+    public void setTonggiolam(int tonggiolam) {
+        this.tonggiolam = tonggiolam;
+    }
+    
+    public String getThongTin()
+    {
+        return "Tên: "+ ten +"\n"
+                +"Tuổi: " + tuoi+"\n"
+                +"Địa chỉ: "+ diachi +"\n"
+                +"Tiền lương: "+ tienluong +"\n"
+                +"Tổng giờ làm: "+ tonggiolam +"\n"
+                +"Thưởng: " +tinhThuong();     
+    }
+    public double tinhThuong()
+    {
+        if (tonggiolam>=200)
+        {
+            return tienluong *0.2;
+        } 
+        else 
+        {
+            if (tonggiolam>=100) 
+            {
+                return tienluong *0.1;
+            } else
+            {
+                return 0;
+            }
+        }
+    }
+}
